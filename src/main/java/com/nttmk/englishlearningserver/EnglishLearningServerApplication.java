d8c124ed56383920096e91673b6df32974b41db3
camapcon0702 <quyle070204@gmail.com>
2025-12-15 23:30:48 +0700

    Initial project

---
+++ src/main/java/com/nttmk/englishlearningserver/EnglishLearningServerApplication.java
@@ -0,0 +1,13 @@
+package com.nttmk.englishlearningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLearningServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishLearningServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nttmk/englishlearningserver/EnglishLearningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttmk.englishlearningserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishLearningServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
