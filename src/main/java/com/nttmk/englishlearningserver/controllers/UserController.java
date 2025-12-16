ce952c9d2d1261a6f1b57c12a5fcbcfd05779fe7
locnpIT <npl2004qn@gmail.com>
2025-12-16 23:09:04 +0700

    up load get current user logged in

--- src/main/java/com/nttmk/englishlearningserver/controllers/UserController.java
+++ src/main/java/com/nttmk/englishlearningserver/controllers/UserController.java
@@ -2,11 +2,13 @@ package com.nttmk.englishlearningserver.controllers;
 
 import com.nttmk.englishlearningserver.dto.LoginDTO;
 import com.nttmk.englishlearningserver.responses.ApiResponse;
+import com.nttmk.englishlearningserver.responses.UserResponse;
 import com.nttmk.englishlearningserver.services.UserService;
 import com.nttmk.englishlearningserver.servicesInterface.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,16 @@ public class UserController {
     public ResponseEntity<ApiResponse<String>> logout() {
         userService.logout();
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(HttpStatus.OK.value(), "Logout success", "Logout Successful"));
+    }
+
+
+    @GetMapping("/current-user")
+    public ResponseEntity<ApiResponse<UserResponse>> getCurrentUserLoggedIn() {
+        UserResponse userResponse = userService.getCurrentUserLoggedIn();
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(
+            new ApiResponse<>(HttpStatus.OK.value(), 
+                "Get current user logged in success", 
+                        userResponse));
     }
 }
--- src/main/java/com/nttmk/englishlearningserver/services/UserService.java
+++ src/main/java/com/nttmk/englishlearningserver/services/UserService.java
@@ -2,6 +2,9 @@ package com.nttmk.englishlearningserver.services;
 
 import com.nttmk.englishlearningserver.dto.LoginDTO;
 import com.nttmk.englishlearningserver.dto.RegisterDTO;
+import com.nttmk.englishlearningserver.exceptions.DataNotFoundException;
+import com.nttmk.englishlearningserver.models.User;
+import com.nttmk.englishlearningserver.repository.UserRepository;
 import com.nttmk.englishlearningserver.responses.UserResponse;
 import com.nttmk.englishlearningserver.servicesInterface.IUserService;
 import org.springframework.security.core.Authentication;
@@ -11,8 +14,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements IUserService {
 
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public void logout() {
         SecurityContextHolder.clearContext();
+    }
+
+    @Override
+    public UserResponse getCurrentUserLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new DataNotFoundException("User not found"));
+        return new UserResponse(user.getId(), user.getFullName(), user.getEmail(), user.getRole(), user.getCreatedAt());
     }
 }
